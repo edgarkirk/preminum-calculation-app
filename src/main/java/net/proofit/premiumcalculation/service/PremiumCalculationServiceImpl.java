5116ab9f4672b62b1470fd79ff6529d643be028f
edgarkirk <edgars.kirkilevics@gmail.com>
2022-03-09 18:21:06 +0100

    added logger

--- src/main/java/net/proofit/premiumcalculation/service/PremiumCalculationServiceImpl.java
+++ src/main/java/net/proofit/premiumcalculation/service/PremiumCalculationServiceImpl.java
@@ -1,6 +1,8 @@
 package net.proofit.premiumcalculation.service;
 
 import net.proofit.premiumcalculation.domain.Policy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,8 @@ import java.math.RoundingMode;
 
 @Service
 public class PremiumCalculationServiceImpl implements PremiumCalculationService {
+
+    private final static Logger log = LoggerFactory.getLogger(PremiumCalculationServiceImpl.class);
 
     private final PremiumCalculationService premiumFireCalculationService;
     private final PremiumCalculationService premiumTheftCalculationService;
@@ -22,7 +26,10 @@ public class PremiumCalculationServiceImpl implements PremiumCalculationService 
     @Override
     public BigDecimal calculate(Policy policy) {
         BigDecimal sumInsuredFire = premiumFireCalculationService.calculate(policy);
+        log.debug("Fire insured sum = {}", sumInsuredFire);
         BigDecimal sumInsuredTheft = premiumTheftCalculationService.calculate(policy);
+        log.debug("Theft insured sum = {}", sumInsuredTheft);
+
         return sumInsuredFire.add(sumInsuredTheft).setScale(2, RoundingMode.HALF_UP);
     }
 }
